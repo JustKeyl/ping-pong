62f67ef46c7146c52e5b33830b5916adb311d35e
JustKeyl <kleewik@gmail.com>
2026-05-03 14:11:40 +0300

    Ура, фон

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 from pygame import *
+
+back = [100, 150, 150]
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+clock = time.Clock()
+FPS = 30
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    display.update()
+    clock.tick(FPS)
